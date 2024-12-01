96a2224d1c39fd46f116c165a3163458a7ea1771
Matthias Hölzl <tc@xantira.com>
2024-12-01 21:33:31 +0100

    Add clean code intro

---
+++ Folien/TypeScript/Completed/Clean Code/01 Was ist Clean Code.ts
@@ -0,0 +1,77 @@
+// -*- coding: utf-8 -*-
+// %% [markdown]
+//
+// <div style="text-align:center; font-size:200%;">
+//   <b>Was ist Clean Code?</b>
+// </div>
+// <br/>
+// <div style="text-align:center; font-size:120%;">Dr. Matthias Hölzl</div>
+// <br/>
+// <div style="text-align:center;">Coding-Akademie München</div>
+// <br/>
+
+// %% [markdown]
+//
+// # Was ist Clean Code?
+//
+// - Clean Code (Robert C. Martin / Object Mentor)
+// - Guter Code
+
+// %% [markdown]
+//
+// ### Grady Booch (OODA)
+//
+// - Clean Code ist einfach und direkt.
+// - Clean Code liest sich wie gut geschriebene Prosa.
+// - Clean Code verdeckt nie die Absicht des Designers, sondern ist voller
+//   klarer Abstraktionen und geradliniger Kontrollflüsse.
+
+// %% [markdown]
+//
+// ### Bjarne Stroustrup (C++)
+//
+// - Ich mag es, wenn mein Code elegant und effizient ist.
+// - Die Logik sollte einfach sein, um es Bugs schwer zu machen, sich zu
+//   verstecken, die Abhängigkeiten minimal, um die Wartung zu erleichtern, die
+//   Fehlerbehandlung vollständig nach einer artikulierten Strategie und die
+//   Leistung nahezu optimal, um die Leute nicht zu verleiten, den Code mit
+//   ad-hoc Optimierungen unordentlich zu machen.
+// - Clean Code macht eine Sache gut.
+
+// %% [markdown]
+//
+// ### Ward Cunningham (Wiki)
+//
+// - Du weißt, dass du sauberen Code liest, wenn jede Routine, die du liest,
+//   ziemlich genau das ist, was du erwartet hast.
+// - Du kannst es schönen Code nennen, wenn der Code auch so aussieht, als wäre
+//   die Sprache für das Problem gemacht worden.
+
+// %% [markdown]
+//
+// ### Michael Feathers (Legacy Code Buch)
+//
+// - Clean Code scheint von jemandem geschrieben worden zu sein, dem der Code
+//   etwas bedeutet ("someone who cares").
+
+// %% [markdown]
+//
+// ## Ziele
+//
+// - Wenige Bugs
+// - Konstante Entwicklungsgeschwindigkeit
+// - Eine Codebasis, mit der es Spaß macht zu arbeiten
+
+// %% [markdown]
+//
+// ## Wie erreichen wir das?
+//
+// In vielen kleinen Schritten!
+
+// %% [markdown]
+//
+// ## Die Boy Scout Rule (für Code)
+//
+// *Hinterlasse den Code besser als du ihn vorgefunden hast.*
+//
+// (Robert C. Martin)
